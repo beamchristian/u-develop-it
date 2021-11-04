09aeafc3a8005e19cb0b8671d1b37b5484cb9543
beamchristian <beamchristian@yahoo.com>
2021-11-04 14:31:05 -0400

    building database calls

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,6 +7,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySql username,
+    user: 'root',
+    // Your MySql password
+    password: 'Sharon13.!',
+    database: 'election',
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
